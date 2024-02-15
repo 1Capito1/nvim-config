b4155e1cb9c8e711902681f7981b21296573fc17
1Capito1 <seth.l.tootell@gmail.com>
2024-02-16 00:48:01 +1100

    added q button protection

--- lua/remap.lua
+++ lua/remap.lua
@@ -4,5 +4,6 @@ local map = vim.api.nvim_set_keymap
 
 map('n', '<leader>pv', ":Ex<CR>", {noremap = true, silent = true})
 map('n', '<leader>y', "\"+y", {noremap = true, silent = true})
+map('n', 'q', "", {noremap = true, silent = true})
 
 map('n', ':', "<cmd>lua require(\"fine-cmdline\").open({default_value = \"\"})<CR>", {})
