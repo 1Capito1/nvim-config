ba8875f088a90f0918ac15dd05b0b30eb9c7de22
1Capito1 <seth.l.tootell@gmail.com>
2024-02-16 00:52:40 +1100

    added neogit remap

--- lua/remap.lua
+++ lua/remap.lua
@@ -7,3 +7,4 @@ map('n', '<leader>y', "\"+y", {noremap = true, silent = true})
 map('n', 'q', "", {noremap = true, silent = true})
 
 map('n', ':', "<cmd>lua require(\"fine-cmdline\").open({default_value = \"\"})<CR>", {})
+map('n', "<leader>ng", ":Neogit<CR>", {noremap = true, silent = true})
